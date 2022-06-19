e04ba293f6ebd2f2b5f55fe330538e4cf9561e8f
Arsen <abdigal.2015@gmail.com>
2022-06-19 12:15:54 +0600

    finished marketplace

--- src/components/marketplace/marketplace.js
+++ src/components/marketplace/marketplace.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import MarketplaceCard from "../cards/marketplace-card";
+import { erc1155Interact } from "../web3/smartcontracts";
 
 import card1 from "./marketplace-img/1.png";
 import card2 from "./marketplace-img/2.png";
@@ -10,39 +11,70 @@ import card3 from "./marketplace-img/3.png";
 import "./marketplace.sass";
 
 const Marketplace = ({ setInMarketplace, setInProfile }) => {
+    const [erc1155tokens, setErc1155tokens] = useState([]);
+    const [userInfluence, setUserInfluence] = useState(0);
+
     useEffect(() => {
         setInMarketplace(true);
         setInProfile(false);
+        let ts = [];
+
+        erc1155Interact().then(async (token) => {
+            token
+                .getPastEvents("TransferSingle", {
+                    fromBlock: 0,
+                    toBlock: "latest",
+                }, async function (error, events) {
+                    await events.forEach(async (ev) => {
+                        const id = ev.returnValues.id;
+
+                        const name = await ((await erc1155Interact()).methods.categories(id)).call();
+                        let available = await ((await erc1155Interact()).methods.totalSupply(id)).call();
+                        const totalSupply = available;
+
+                        token.getPastEvents("TransferSingle", {
+                            filter: { id: id, },
+                            fromBlock: 0,
+                            toBlock: "latest",
+                        }, async function (error, events) {
+                            await events.forEach(async (ev) => {
+                                const to = ev.returnValues.to;
+                                const balance = await ((await erc1155Interact()).methods.balanceOf(to, id));
+                                available -= balance;
+                            });
+                        });
+
+                        const card = {
+                            name: name,
+                            totalSupply: totalSupply,
+                            available: available
+                        };
+
+                        ts.push(card);
+                    });
+                })
+        });
+
+        setErc1155tokens(ts);
     }, []);
+
+    let cards = erc1155tokens.filter((_, i) => i % 2).map((token, index) => {
+        return <Link to="/dao" style={{ textDecoration: "none" }}>
+            <MarketplaceCard
+                key={index}
+                name={token.name}
+                available={token.available}
+                max={token.totalSupply}
+                img={card1}
+            />
+        </Link>
+    })
 
     return (
         <div className="marketplace">
             <div className="marketplace-header">[explore]</div>
             <div className="marketplace-container">
-                <Link to="/dao" style={{ textDecoration: "none" }}>
-                    <MarketplaceCard
-                        name="Conquer the world"
-                        available={276}
-                        max={1000}
-                        img={card1}
-                    />
-                </Link>
-                <Link to="/dao" style={{ textDecoration: "none" }}>
-                    <MarketplaceCard
-                        name="Peal Avocado"
-                        available={276}
-                        max={1000}
-                        img={card3}
-                    />
-                </Link>
-                <Link to="/dao" style={{ textDecoration: "none" }}>
-                    <MarketplaceCard
-                        name="Cut avocado"
-                        available={276}
-                        max={1000}
-                        img={card2}
-                    />
-                </Link>
+                {cards}
             </div>
         </div>
     );
